ef0f900edbc58f50f66838eee9b7816c699624fc
jesus <jmmartinezbaute@unicesar.edu.co>
2026-09-05 13:44:34 -0500

    feat: implement Console class in model layer

--- gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/model/Console.java
+++ gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/model/Console.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.gamezone.gamezone.unicesar.model;
 
-/**
- *
- * @author HP
- */
-public class Console {
-    
+
+public class Console extends Product {
+   private String brand;
+   private String model;
+   private String generation;
+
+    public Console() {
+    }
+
+    public Console(String brand, String model, String generation, String id, String title, double price, int stock) {
+        super(id, title, price, stock);
+        this.brand = brand;
+        this.model = model;
+        this.generation = generation;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+   
+    @Override
+    public String getFullDescription(){
+       return String.format("Console: [ID: %s, Title: %s, Price: $%.2f, Stock: %d, Brand: %s, Model: %s, Generation: %s]",getId(), getTitle(), getPrice(), getStock(), brand, model,generation);
+    } 
 }
