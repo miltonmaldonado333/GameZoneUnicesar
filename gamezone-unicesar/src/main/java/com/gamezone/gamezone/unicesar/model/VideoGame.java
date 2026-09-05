0b00645e78fe768194b083de1dd2a8293e249303
jesus <jmmartinezbaute@unicesar.edu.co>
2026-09-05 13:34:14 -0500

    feat:implement VideoGame class in model layer

--- gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/model/VideoGame.java
+++ gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/model/VideoGame.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.gamezone.gamezone.unicesar.model;
 
+package com.gamezone.gamezone.unicesar.model;
 /**
- *
- * @author HP
+ * a class representing a VideoGame Product
+ * @author JESUS
  */
-public class VideoGame {
+
+public class VideoGame extends Product {
+   private String platform;
+   private String genre;
+   private String ageRating;
+
+    public VideoGame() {
+    }
+
+    public VideoGame(String platform, String genre, String ageRating) {
+        this.platform = platform;
+        this.genre = genre;
+        this.ageRating = ageRating;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAgeRating() {
+        return ageRating;
+    }
+
+    public void setAgeRating(String ageRating) {
+        this.ageRating = ageRating;
+    }
+   
+    @Override
+    public String getFullDescription(){
+        return String.format("Console: [ID %s, Title: %s, price: $%.2f, Stock: %d, Brand: %s,Model: %s, Generation: %s] ",getId(), getTitle(), getPrice(), getStock(), platform, genre, ageRating);
+    }
+   
+    
     
 }
