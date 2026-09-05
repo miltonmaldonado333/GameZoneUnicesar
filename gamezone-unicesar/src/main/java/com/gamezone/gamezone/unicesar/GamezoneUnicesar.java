d50d4a4c076ed75935d1ab43c870aa3103ddd510
jesus <jmmartinezbaute@unicesar.edu.co>
2026-09-05 12:59:08 -0500

    chore: initial project structure setup for product module

---
+++ gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/GamezoneUnicesar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gamezone.gamezone.unicesar;
+
+/**
+ *
+ * @author HP
+ */
+public class GamezoneUnicesar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/model/VideoGame.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gamezone.gamezone.unicesar.model;
+
+/**
+ *
+ * @author HP
+ */
+public class VideoGame {
+    
+}
