79c8d1cd756f97a7ad400ef20801c5ad984d3156
jesus <jmmartinezbaute@unicesar.edu.co>
2026-09-08 14:46:11 -0500

    feat: implement loadProducts in FileProductRepository

---
+++ gamezone-unicesar/src/main/java/com/gamezone/gamezone/unicesar/persistence/FileProductRepository.java
@@ -0,0 +1,72 @@
+package com.gamezone.gamezone.unicesar.persistence;
+
+import com.gamezone.gamezone.unicesar.model.Console;
+import com.gamezone.gamezone.unicesar.model.Product;
+import com.gamezone.gamezone.unicesar.model.VideoGame;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Concrete implementation of ProductRepository that handles file-based
+ * persistence.
+ *
+ * @author Jesus
+ */
+public class FileProductRepository implements ProductRepository {
+
+    private static final String FILE_PATH = "products.txt";
+
+    @Override
+    public List<Product> loadProducts() {
+        List<Product> products = new ArrayList();
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return products;
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    continue;
+                }
+                String data[] = line.split(";");
+                String type = data[0];
+                if (type.equalsIgnoreCase("VideoGame")) {
+                    String platform = data[1];
+                    String genre = data[2];
+                    String ageRating = data[3];
+                    String id = data[4];
+                    String title = data[5];
+                    double price = Double.parseDouble(data[6]);
+                    int stock = Integer.parseInt(data[7]);
+                    products.add(new VideoGame(platform, genre, ageRating, id, title, price, stock));
+                } else if (type.equalsIgnoreCase("Console")) {
+                    String brand = data[1];
+                    String model = data[2];
+                    String generation = data[3];
+                    String id = data[4];
+                    String title = data[5];
+                    double price = Double.parseDouble(data[6]);
+                    int stock = Integer.parseInt(data[7]);
+                    products.add(new Console(brand, model, generation, id, title, price, stock));
+                }
+
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading products file: " + e.getMessage());
+        }
+        return products;
+
+    }
+
+    @Override
+    public void saveProducts(List<Product> products) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
