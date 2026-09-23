76bfad0d19fd38f8b31a5b648eb483939edf7bac
jesus <jmmartinezbaute@unicesar.edu.co>
2026-09-23 13:05:45 -0500

    feat adding categorydiscount class

---
+++ src/main/java/com/gamezone/model/CategoryDiscount.java
@@ -0,0 +1,45 @@
+package com.gamezone.model;
+
+import java.time.LocalDate;
+
+public class CategoryDiscount extends Promotion {
+
+    private double percentage;
+    private String targetCategory;
+
+    public CategoryDiscount(double percentage, String targetCategory, String id, String name, LocalDate startDate, LocalDate endDate) {
+        super(id, name, startDate, endDate);
+        this.percentage = percentage;
+        this.targetCategory = targetCategory;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public String getTargetCategory() {
+        return targetCategory;
+    }
+
+    public void setTargetCategory(String targetCategory) {
+        this.targetCategory = targetCategory;
+    }
+
+    @Override
+    public double calculateDiscount(Sale sale) {
+        double applicableSubtotal = 0.0;
+        for (Product product : sale.getProducts()) {
+            if ("VIDEOGAME".equalsIgnoreCase(targetCategory) && product instanceof VideoGame) {
+                applicableSubtotal += product.getPrice();
+            } else if ("CONSOLE".equalsIgnoreCase(targetCategory) && product instanceof Console) {
+                applicableSubtotal += product.getPrice();
+            }
+
+        }
+        return applicableSubtotal * (percentage / 100);
+    }
+}
