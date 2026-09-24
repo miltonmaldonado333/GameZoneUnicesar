aaf85c6b1da38e6e9d4eb6aecd3e6c6ea07ea5e7
luismcorzoc-droid <luismcorzoc@gmail.com>
2026-09-23 19:33:17 -0500

    fix: correct getters and setters formatting in Promotion class

--- src/main/java/com/gamezone/model/Promotion.java
+++ src/main/java/com/gamezone/model/Promotion.java
@@ -36,4 +36,35 @@ public abstract class Promotion {
     public abstract double calculateDiscount(Sale sale);
 
     // Genera aquí los Getters y Setters para id, name, startDate y endDate
+    public String getId() {
+    return id;
+}
+
+public void setId(String id) {
+    this.id = id;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public LocalDate getStartDate() {
+    return startDate;
+}
+
+public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+}
+
+public LocalDate getEndDate() {
+    return endDate;
+}
+
+public void setEndDate(LocalDate endDate) {
+    this.endDate = endDate;
+}
 }
